dd389639cd66fbbbed3637ca725f5cabe2ef589c
giorgi abzhandadze <giorgi.abjandadze@gmail.com>
2024-06-03 11:29:01 +0400

    commit

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static  IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration =>
+        configuration.RegisterServicesFromAssemblies(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
